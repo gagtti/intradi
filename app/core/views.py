5a5130bc1e5adeca1b3c59f32a034be2a6e402d8
Gustavo Gosen <gagosen@gmail.com>
2022-03-21 11:37:07 -0300

    Commit app core funcionando antes de configurar Gunicorn

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,9 @@
+from re import template
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomePageView(TemplateView):
+
+    template_name = "core/home.html"
 
 # Create your views here.
